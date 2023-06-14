437346660b8dd711eeccdafe99d088f1c738b54b
Abhishek <abhishek.singh2_cs19@gla.ac.in>
2023-06-14 05:55:51 +0530

    mongo starting command

---
+++ mongodb.py
@@ -0,0 +1,13 @@
+import pymongo
+uri = "mongodb+srv://abhishek:devilnat007@cluster0.nualpoa.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(uri)
+db = client.test
+print(db)
+a = {
+        "name": 'abhishek',
+        "roll": 191500039,
+        "email": 'abhishek.singh2_cs19@gmal.com'
+    }
+mongodb = client['mongotest']
+coll = mongodb['test']
+coll.insert_one(a)
